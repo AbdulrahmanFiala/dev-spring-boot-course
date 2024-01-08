bad8229f20a85f5e3aebebd6122fc3b1f9858a59
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-01-09 00:47:01 +0200

    example of constructor injection

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/DemoController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach thecoach){
+        myCoach = thecoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
