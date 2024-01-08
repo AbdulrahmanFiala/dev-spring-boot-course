16f161aaea99ed8a5e1f5bf853f9a0cfa1bf0340
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-01-09 01:03:23 +0200

    orgnizing files into packages

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -4,6 +4,9 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(
+//        scanBasePackages = {"com.luv2code.springboot.demo.mycoolapp","com.luv2code.util"}
+//)
 @SpringBootApplication
 public class MycoolappApplication {
 
