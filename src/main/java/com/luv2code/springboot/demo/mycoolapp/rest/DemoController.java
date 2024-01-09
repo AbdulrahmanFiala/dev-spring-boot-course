1f8a5845d4ea9867e22dc02724515c99d35897f0
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-01-09 18:29:46 +0200

    example of Qualifer annotation

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import com.luv2code.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,15 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
     // define a constructor for dependency injection
-//    @Autowired
-//    public DemoController(Coach thecoach){
-//        myCoach = thecoach;
-//    }
-
     @Autowired
-    public void setCoach(Coach thecoach) {
+    public DemoController(@Qualifier("trackCoach") Coach thecoach){
         myCoach = thecoach;
     }
+    // define a setter method for dependency injection
+//    @Autowired
+//    public void setCoach(Coach thecoach) {
+//        myCoach = thecoach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
